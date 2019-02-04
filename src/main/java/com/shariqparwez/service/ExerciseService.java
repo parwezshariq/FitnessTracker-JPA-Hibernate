ca5dffafc84111d55d9cdcd8e6022515a8ff31f4
shariqparwez <shariqparwez@outlook.com>
2019-02-04 12:06:41 +0530

    Initial Commit

---
+++ src/main/java/com/shariqparwez/service/ExerciseService.java
@@ -0,0 +1,11 @@
+package com.shariqparwez.service;
+
+import java.util.List;
+
+import com.shariqparwez.model.Activity;
+
+public interface ExerciseService {
+
+	List<Activity> findAllActivities();
+
+}
