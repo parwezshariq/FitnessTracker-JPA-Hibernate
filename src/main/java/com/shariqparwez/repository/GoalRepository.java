18877bf6353e37c0776f8f5a62a847e306eed65f
shariqparwez <shariqparwez@outlook.com>
2019-02-05 13:05:49 +0530

    Repository Configuration

---
+++ src/main/java/com/shariqparwez/repository/GoalRepository.java
@@ -0,0 +1,9 @@
+package com.shariqparwez.repository;
+
+import com.shariqparwez.model.Goal;
+
+public interface GoalRepository {
+	
+	Goal save(Goal goal);
+
+}
--- src/main/java/com/shariqparwez/service/GoalServiceImpl.java
+++ src/main/java/com/shariqparwez/service/GoalServiceImpl.java
@@ -1,14 +1,19 @@
 package com.shariqparwez.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shariqparwez.model.Goal;
+import com.shariqparwez.repository.GoalRepository;
 
 @Service("goalService")
 public class GoalServiceImpl implements GoalService {
 
+	@Autowired
+	private GoalRepository goalRepository;
+	
 	public Goal save(Goal goal) {
-		return null;
+		return goalRepository.save(goal);
 	}
 
 }
