da130c26630787edf893947d9c9a0ff307754324
shariqparwez <shariqparwez@outlook.com>
2019-02-05 15:25:11 +0530

    End-to-End configuration

--- src/main/java/com/shariqparwez/service/GoalServiceImpl.java
+++ src/main/java/com/shariqparwez/service/GoalServiceImpl.java
@@ -2,6 +2,7 @@ package com.shariqparwez.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.shariqparwez.model.Goal;
 import com.shariqparwez.repository.GoalRepository;
@@ -12,6 +13,7 @@ public class GoalServiceImpl implements GoalService {
 	@Autowired
 	private GoalRepository goalRepository;
 	
+	@Transactional
 	public Goal save(Goal goal) {
 		return goalRepository.save(goal);
 	}
