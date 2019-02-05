961cbabdd154b414e434e002103986d0c2cedab1
shariqparwez <shariqparwez@outlook.com>
2019-02-05 12:58:07 +0530

    Service Configuration

---
+++ src/main/java/com/shariqparwez/service/GoalService.java
@@ -0,0 +1,9 @@
+package com.shariqparwez.service;
+
+import com.shariqparwez.model.Goal;
+
+public interface GoalService {
+
+	Goal save(Goal goal);
+	
+}
---
+++ src/main/java/com/shariqparwez/service/GoalServiceImpl.java
@@ -0,0 +1,14 @@
+package com.shariqparwez.service;
+
+import org.springframework.stereotype.Service;
+
+import com.shariqparwez.model.Goal;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+	public Goal save(Goal goal) {
+		return null;
+	}
+
+}
